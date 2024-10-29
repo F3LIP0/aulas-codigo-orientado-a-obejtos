ba2341b1a7253b08cb56f956171a1488b9af8e1d
F3LIP0 <felipesakato@gmail.com>
2024-10-29 00:23:44 +0000

    comentario

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int idade;
+    cout << "Digite sua idade: ";
+    cin >> idade;
+    if (idade >= 18) {
+        cout << "Voce pode ser preso";
+    }
+    else {
+        cout << "Voce vai pra FASE";
+    }
+    
+    
+
+    return 0;
+
+}
