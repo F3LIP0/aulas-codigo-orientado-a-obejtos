a1c5537b02d962f4a7131487466d9a1a8df6f858
F3LIP0 <felipesakato@gmail.com>
2024-11-01 18:09:03 +0000

    biblioteca boo

---
+++ calpy.py
@@ -0,0 +1,5 @@
+def main ():
+    while True:
+        num1 = float(input("Digite o primeiro numero: "))
+        num2 = float(input("Digite o segundo numero: "))
+        operacao = (input("Digite uma operacao (+, -, *, /): "))
